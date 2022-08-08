bc8c2b8dcd0ee5439995a8096829f519894c9117
Asadbek <dasturchi6678@gmail.com>
2022-08-08 19:00:45 +0500

    oxiri

--- lesson1/lib/result.dart
+++ lesson1/lib/result.dart
@@ -2,14 +2,26 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int fullScore;
-  const Result({required this.fullScore, Key? key}) : super(key: key);
+  final VoidCallback reset;
+  const Result({required this.fullScore, required this.reset, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        "Jami achko: $fullScore",
-        style: const TextStyle(fontSize: 20),
+      child: Column(
+        children: [
+          Text(
+            "Jami achko: $fullScore",
+            style: const TextStyle(fontSize: 20),
+          ),
+          TextButton(
+              onPressed: reset,
+              child: const Text(
+                'Qaytadan urinish',
+                style: TextStyle(color: Colors.blue),
+              )),
+        ],
       ),
     );
   }
